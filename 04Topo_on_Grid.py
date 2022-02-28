04d5a2e0c35fb1f8f5cd4e49be7f8825792c4d0d
Andres Laverde <andres20@ru.is>
2022-02-28 09:53:47 +0000

    Modified scripts comments and topography-rock grid generated

--- 04Topo_on_Grid.py
+++ 04Topo_on_Grid.py
@@ -3,15 +3,14 @@ from t2data import *
 from t2incons import *
 from t2listing import *
 
-print ('hello')
+print ('Adding topography to optimized grid')
 #read model grid
-geo=mulgrid('02b_grid.dat')
+geo=mulgrid('03_grid.dat')
 
 ##############################################################################
 
 #this fits the surface to xyz, note can fit specific cols if req'd
 topo_data=np.loadtxt('Topography_Cupiagua.dat',delimiter=',')
-print ('hello_again')
 geo.fit_surface(topo_data,1.0,1.0)
 #write geometry file
 geo.write('04_grid.dat')
--- 05create_flow_model.py
+++ 05create_flow_model.py
@@ -4,10 +4,10 @@ from t2incons import *
 from t2listing import *
 
 #open geometry file  BEN check the name!
-geo=mulgrid('04_grid_atm.dat')
+geo=mulgrid('04_grid.dat')
 #open input data file
  
-dat=t2data('05AnyOldTOUGH2File.dat')
+dat=t2data('05_1D_21_eos1.dat')
 
 dat.grid=t2grid().fromgeo(geo)
 
@@ -16,9 +16,5 @@ dat.grid=t2grid().fromgeo(geo)
 
 #write input file
 dat.write('001.dat')
-#probably best to now import this to Leapfrog and create flow model with rock 
-#types on it, and export that to get heat generators 
-
-
-
-                                   
+#probably best to now import this to Leapfrog and create flow model with rock
+#types on it, and export that to get heat generators
