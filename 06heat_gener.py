9bc2a2bf78c29f2f50d0d26d1f2e7500701c2f2f
Andres Laverde <andres20@ru.is>
2022-03-02 09:15:01 +0000

    New grid for heat generation

--- 06heat_gener.py
+++ 06heat_gener.py
@@ -1,9 +1,9 @@
 from t2data import *
 
-geo=mulgrid('04_grid.dat')
+geo=mulgrid('04_rock_grid_geometry.dat')
 #import input file##############################################################
 #this can be input after you have put the rock types in Leapfrog & exported
-dat=t2data('0001WithRockTypes.dat')
+dat=t2data('04_rock_grid.dat')
 dat.clear_generators
 #go to bottom layer:
 layer=geo.layerlist[-1]
@@ -11,7 +11,7 @@ layer=geo.layerlist[-1]
 #warmed up, and then make a new input with 0.10 that should be enough background
 #heat.  Add the convective upflow yourself.  Refer to Page 156 onwards of TOUGH2
 #manual.This is Appendix which deals with the format of the entire input file.  
-q=0.10
+q=0.03
 for col in geo.columnlist:
    blockname=geo.block_name(layer.name,col.name)
    gen=t2generator(name=col.name+' 1',block=blockname,type='HEAT',gx=q*col.area)
